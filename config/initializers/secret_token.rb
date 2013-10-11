5eb538c0761f6498dce24410ccdd6049a322bfdd
Anita Jaszewska <anita.jaszewska@wimdu.com>
2013-10-11 18:18:07 +0200

    init testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testing::Application.config.secret_token = '1629feef19cf80e84cf0382edec10e7c7cf7b4a4a23f4ee013aec720638ad1c1877aa03cd0f6c277d7252c1b32c3e5435f43abb9b70a091983940eb24eb15d98'
